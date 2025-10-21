b7fda6bcd9e2ea057807a1964bfb481d5adb4bd5
arthurfelipeparaujo-ai <arthurfelipe.p.araujo@gmail.com>
2025-10-21 20:46:03 -0300

    Add files via upload

---
+++ main (1).cs
@@ -0,0 +1,20 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+using System;
+class HelloWorld {
+  static void Main() {
+    Console.WriteLine("Hello World");
+  }
+  int downloadsDiarios
+  enquanto ("for e igual a 0 faca")
+  leia downloadsDiarios
+  enquanto for igual a 2 faca
+  para quando  chegar a 
+  
+}
